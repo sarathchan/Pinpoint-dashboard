397c366af8e63091f0d67805aa6b1198f78fe7b8
sarathchan <sarathchan20@gmail.com>
2023-03-14 09:26:27 +0530

    chan

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,36 @@
+export type AmplifyDependentResourcesAttributes = {
+  "analytics": {
+    "amplifydashboard": {
+      "Id": "string",
+      "Region": "string",
+      "appName": "string"
+    }
+  },
+  "api": {
+    "api62087d45": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "auth": {
+    "amplifydashboard": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "amplifydashboard89b4e93b": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import Login from './Login/Login';
 import './App.css';
 import {Routes,Route} from "react-router-dom";
+import {Analytics} from 'aws-amplify'
+import { useEffect } from 'react';
 import Dashboard from './Dashboard/Dashboard';
 
 function App() {
+  useEffect(() => {
+    Analytics.record("Home Page Visit")
+    Analytics.record({
+      name: "UserSignIn",
+      attributes:{
+        userName:"chan"
+      }
+    })
+  }, [])
   return (
     <div className="App">
       <Routes>
